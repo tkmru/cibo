22ed3996818e671d08af1c609fa2ad9f7a983e26
tkmru <i.am.tkmru@gmail.com>
2017-06-01 23:51:04 +0900

    set EFLAGS func

--- cibo.go
+++ cibo.go
@@ -67,8 +67,95 @@ func (r Registers) dump() {
 	}
 }
 
+func (r *Registers) setCF() {
+	// Carry Flag (0 bit)
+	r.EFLAGS = r.EFLAGS | 1
+}
+
+func (r *Registers) setPF() {
+	// Parity Flag (2bit)
+	r.EFLAGS = r.EFLAGS | 4
+}
+
+func (r *Registers) setAF() {
+	// Adjust Flag (4bit)
+	r.EFLAGS = r.EFLAGS | 16
+}
+
+func (r *Registers) setZF() {
+	// Zero Flag (6bit)
+	r.EFLAGS = r.EFLAGS | 64
+}
+
+func (r *Registers) setSF() {
+	// Sign Flag (7bit)
+	r.EFLAGS = r.EFLAGS | 128
+}
+
+func (r *Registers) setTF() {
+	// Trap Flag (8bit)
+	r.EFLAGS = r.EFLAGS | 256
+}
+
+func (r *Registers) setIF() {
+	// Interrupt Enable Flag (9bit)
+	r.EFLAGS = r.EFLAGS | 512
+}
+
+func (r *Registers) setDF() {
+	// Direction Flag (10bit)
+	r.EFLAGS = r.EFLAGS | 1024
+}
+
+func (r *Registers) setOF() {
+	// Overflow Flag (11bit)
+	r.EFLAGS = r.EFLAGS | 2048
+}
+
+func (r *Registers) setIOPL() {
+	// I/O Privilege Level Field (12-13bit)
+	r.EFLAGS = r.EFLAGS | 4096 // TODO: fix later
+}
+
+func (r *Registers) setNT() {
+	// Nested Task Flag (14bit)
+	r.EFLAGS = r.EFLAGS | 16384
+}
+
+func (r *Registers) setRF() {
+	// Resume Flag (16bit)
+	r.EFLAGS = r.EFLAGS | 65536
+}
+
+func (r *Registers) setVM() {
+	// Virtual x86 Mode Flag (17bit)
+	r.EFLAGS = r.EFLAGS | 131072
+}
+
+func (r *Registers) setAC() {
+	// Alignment Check Flag (18bit)
+	r.EFLAGS = r.EFLAGS | 262144
+}
+
+func (r *Registers) setVIF() {
+	// Virtual Interrupt Flag (19bit)
+	r.EFLAGS = r.EFLAGS | 524288
+}
+
+func (r *Registers) setVIP() {
+	// Virtual Interrupt Pending Flag (20bit)
+	r.EFLAGS = r.EFLAGS | 1048576
+}
+
+func (r *Registers) setID() {
+	// Identification Flag (21bit)
+	r.EFLAGS = r.EFLAGS | 2097152
+}
+
 func main() {
 	r := Registers{}
 	r.init()
+	r.setCF()
+	r.setTF()
 	r.dump()
 }
