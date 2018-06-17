ecc10d06e48113b109b49c21cf5aa15d7a10c518
tkmru <i.am.tkmru@gmail.com>
2018-06-18 03:00:51 +0900

    add MMX

--- cibo/register.go
+++ cibo/register.go
@@ -25,7 +25,7 @@ type Registers struct {
 	R15 uint64
 	// Instruction Register
 	RIP uint64
-	// Segment Register
+	// Segment Registers
 	CS uint16
 	DS uint16
 	SS uint16
@@ -34,6 +34,37 @@ type Registers struct {
 	GS uint16
 	// FLAGS Register
 	RFLAGS uint64
+	// MMX registers (MM0 through MM7)
+	MM0 uint64
+	MM1 uint64
+	MM2 uint64
+	MM3 uint64
+	MM4 uint64
+	MM5 uint64
+	MM6 uint64
+	MM7 uint64
+  // TODO: XMM registers (XMM0 through XMM15) and the MXCSR register
+  // uint128 doesn't existed
+
+	// Control Registers
+	CR0  uint64
+	CR1  uint64
+	CR2  uint64
+	CR3  uint64
+	CR4  uint64
+	CR5  uint64
+	CR6  uint64
+	CR7  uint64
+	CR8  uint64
+	CR9  uint64
+	CR10 uint64
+	CR11 uint64
+	CR12 uint64
+	CR13 uint64
+	CR14 uint64
+	CR15 uint64
+	// Extended Feature Enable Register
+  IA32_EFER uint64
 }
 
 func (r *Registers) Init() {
@@ -64,16 +95,16 @@ func (r Registers) Dump() {
 	v := reflect.ValueOf(&r).Elem()
 	t := v.Type()
 
-	for i := 0; i < v.NumField(); i++ {
+	for i := 0; i < 24; i++ {
 		registerName := t.Field(i).Name
 		registerValue := v.Field(i).Interface()
 
 		switch registerName {
 		case "RFLAGS":
-			fmt.Printf("%2d: %s = %d (%064b)\n",
+			fmt.Printf("%02d: %s = %d (%064b)\n",
 				i+1, registerName, registerValue, registerValue)
 		default:
-			fmt.Printf("%d: %s = %d\n",
+			fmt.Printf("%02d: %s = %d\n",
 				i+1, registerName, registerValue)
 		}
 	}
